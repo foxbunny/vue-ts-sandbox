c2fdc203145ed247f0cdb9eea72bfa674d345686
Hajime Yamasaki Vukelic <hayavuk@gmail.com>
2017-03-25 23:17:30 +0100

    Make prop util func curried

--- src/util/prop.ts
+++ src/util/prop.ts
@@ -1,6 +1,8 @@
+import curried from './curried'
+
 /**
  * Return a property matching the given key
  */
-const prop = (key: string, obj: any): any => obj[key]
+const prop = curried((key: string, obj: any): any => obj[key])
 
 export default prop
